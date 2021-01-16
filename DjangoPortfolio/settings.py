a4cbd87671d818ddf04708fdd18ba0bac75fba8c
Jacky Wang <jacky200218@gmail.com>
2021-01-16 11:07:57 -0800

    styling

--- DjangoPortfolio/settings.py
+++ DjangoPortfolio/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['django-cs-portfolio.herokuapp.com/', 'www.jiaqiwang1118.com/']
 
