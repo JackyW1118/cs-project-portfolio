6f00547cddb23637e2a7549122a727e64aceef95
Jacky Wang <jacky200218@gmail.com>
2021-01-15 22:22:00 -0800

    styling

--- myprojects/models.py
+++ myprojects/models.py
@@ -4,18 +4,20 @@ from django.utils import timezone
 # each model is a project
 class Project(models.Model):
     title = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now, blank=True)
     short_description = models.TextField(blank=True)
     description = models.TextField(verbose_name="Description")
     project_link = models.URLField(null=True, blank=True)
     front_image = models.ImageField(upload_to='proj_frontimages',verbose_name="Thumbnail")
+    large_image = models.ImageField(upload_to='proj_frontimages', blank=True)
     technical_details = models.TextField(verbose_name="Technical Details", null=True)
     slug = models.SlugField(unique=True, blank=True)
-
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        self.slug = f"{self.title}".replace(' ','').lower()
+        if self.slug is None:
+            self.slug = f"{self.title}".replace(' ','').lower()
         super(Project, self).save(*args, **kwargs)
 
 class ProjectUpdate(models.Model):
